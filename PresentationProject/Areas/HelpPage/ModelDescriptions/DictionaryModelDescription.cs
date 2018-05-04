6b4a93633af6930986ed7be971a9589b111bb136
Tirzah Morrison <tirzahmorrison@icloud.com>
2018-05-04 08:41:28 -0400

    Add project files.

---
+++ PresentationProject/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace PresentationProject.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ PresentationProject/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace PresentationProject.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
